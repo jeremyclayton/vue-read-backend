50d3350c37c8002c838b41c74043f24731892b35
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-04-20 11:59:10 -0600

    added put route

--- routes/books.js
+++ routes/books.js
@@ -42,4 +42,31 @@ router.get('/:id', (req, res, next) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+        knex('book')
+            .select(
+                'book.id as book_id',
+                'book.title',
+                'book.genre',
+                'book.description',
+                'book.cover_url',
+                'author.id as author_id',
+                'author.first_name',
+                'author.last_name')
+            .join('book_author', 'book_author.book_id', 'book.id')
+            .join('author', 'author.id', 'book_author.author_id')
+            .where('book_id', req.params.id)
+            .update({
+                title: req.body.title,
+                genre: req.body.genre,
+                description: req.body.description,
+                cover_url: req.body.cover_url,
+            }, 'id').then(data => {
+                const reformatted = reformate(data);
+                res.json(reformatted)
+            });
+
+});
+
 module.exports = router;
